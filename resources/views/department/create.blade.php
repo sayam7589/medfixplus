db8b685fa58e55603077db1802efdc36a563a3ed
Ramune <mahutkeerati@gmail.com>
2024-10-10 11:11:26 +0700

    .

--- resources/views/department/create.blade.php
+++ resources/views/department/create.blade.php
@@ -83,7 +83,7 @@
             <!-- Default box -->
             <div class="card">
                 <div class="card-header">
-                    <h3 class="card-title">เพิ่มกอง/เเผนก</h3>
+                    <h3 class="card-title">หน่วยปฏิบัติงาน</h3>
                     <div class="card-tools">
                         <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
                             <i class="fas fa-minus"></i>
