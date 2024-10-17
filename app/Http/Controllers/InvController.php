1eaaeb54c4b23d360361be76645cf9785a17a1f8
Ramune <mahutkeerati@gmail.com>
2024-10-17 09:22:23 +0700

    .

--- app/Http/Controllers/InvController.php
+++ app/Http/Controllers/InvController.php
@@ -12,7 +12,6 @@ use SimpleSoftwareIO\QrCode\Facades\QrCode;
 use App\Models\PersonalHasInv;
 use App\Models\Medfix;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Log;
 Use Alert;
 use App\Models\Issue;
 use App\Models\Solving;
@@ -178,7 +177,7 @@ class InvController extends Controller
         }
 
         $qrcodes = $inventories->mapWithKeys(function ($inventory) {
-            $url = route('inventorys.qr', ['id' => $inventory->id]); // No need for manual URL modification
+            $url = route('inventorys.qr', ['id' => $inventory->id], true);// No need for manual URL modification
             return [$inventory->id => QrCode::size(250)->generate($url)];
         });
 
