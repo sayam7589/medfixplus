8019d0e9ae4cefb429f8fe0a8e99dc67d29b4910
Ramune <mahutkeerati@gmail.com>
2024-11-06 16:06:07 +0700

    .

--- resources/views/layouts/qrcode.blade.php
+++ resources/views/layouts/qrcode.blade.php
@@ -21,7 +21,6 @@
             flex-direction: column;
             align-items: center;
             text-align: center;
-            margin: 5mm 0 5mm 0; /* Space around cards */
             page-break-after: always; /* Page break after each card */
         }
 
