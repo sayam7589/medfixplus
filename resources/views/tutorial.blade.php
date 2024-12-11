ecf69c0d3351f53e27ee52b40d9188633df4c20e
Ramune <mahutkeerati@gmail.com>
2024-12-11 11:44:31 +0700

    img

--- resources/views/tutorial.blade.php
+++ resources/views/tutorial.blade.php
@@ -10,8 +10,12 @@
 <body class="bg-light d-flex justify-content-center align-items-center vh-100">
     <div class="container text-center">
         <!-- Responsive Image -->
-        <img src="your-image.jpg" alt="Responsive Example" class="img-fluid rounded shadow">
         <p class="mt-3 text-muted">คู่มือการใช้งานระบบเเจ้งซ่อม</p>
+        <img src="/img/p1_0.jpg" alt="Responsive Example" class="img-fluid rounded shadow">
+        <img src="/img/p2_0.jpg" alt="Responsive Example" class="img-fluid rounded shadow">
+        <img src="/img/p3_0.jpg" alt="Responsive Example" class="img-fluid rounded shadow">
+        <img src="/img/p4_0.jpg" alt="Responsive Example" class="img-fluid rounded shadow">
+        <img src="/img/p5_0.jpg" alt="Responsive Example" class="img-fluid rounded shadow">
     </div>
     <!-- Bootstrap JS Bundle -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
