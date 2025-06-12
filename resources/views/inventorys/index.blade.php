ff4d199ce3fb0845fe5daf9c52c803fcb2a28d3c
IRamuneI <mahutkeerati@gmail.com>
2025-06-12 08:54:46 +0700

    //test pdf

--- resources/views/inventorys/index.blade.php
+++ resources/views/inventorys/index.blade.php
@@ -256,7 +256,7 @@ function generateTestPDF() {
         customize: function (doc) {
             doc.defaultStyle = {
                 font: 'THSarabunNew',  // ✅ ต้องตรงกับชื่อฟอนต์ใน pdfMake.fonts
-                fontSize: 12           // ✅ ปรับขนาดให้อ่านง่าย
+                fontSize: 12,           // ✅ ปรับขนาดให้อ่านง่าย
                 alignment: 'center'
             };
         }
