86d6d074ebf43a9007b12a20be41ffa561062d8c
IRamuneI <mahutkeerati@gmail.com>
2025-06-12 09:56:47 +0700

    //test

--- resources/views/inventorys/index.blade.php
+++ resources/views/inventorys/index.blade.php
@@ -6,7 +6,7 @@
     <link rel="stylesheet" href="{{ asset('adminlte/plugins/datatables-responsive/css/responsive.bootstrap4.min.css') }}">
     <link rel="stylesheet" href="{{ asset('adminlte/plugins/datatables-buttons/css/buttons.bootstrap4.min.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-
+    <title fontsize="24">รายงานบัญชีสินทรัพย์</title>
     <style>
     .content {
         display: none;
