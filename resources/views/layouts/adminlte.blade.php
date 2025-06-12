9f8e45e970b908e6aeabf6c201d8ab32e2a390f4
IRamuneI <mahutkeerati@gmail.com>
2025-06-12 09:49:33 +0700

    //re move title from adminlte

--- resources/views/layouts/adminlte.blade.php
+++ resources/views/layouts/adminlte.blade.php
@@ -4,7 +4,6 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>AdminLTE 3 | Blank Page</title>
 
     <!-- Google Font: Source Sans Pro -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
